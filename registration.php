6e0e0d9b4e0198910dda6e705f8774b38a7ced00
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:19:16 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'clutch_DummyPkg',
+    __DIR__
+);
